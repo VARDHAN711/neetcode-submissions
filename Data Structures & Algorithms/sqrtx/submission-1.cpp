d3e6963305eaba9f3925c0e20812740b46e13c43
Mourya Vardhan <145637651+VARDHAN711@users.noreply.github.com>
2026-06-29 15:12:39 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long long guess = x;
+        while (guess * guess > x)
+            guess = (guess + x / guess) / 2;
+
+        return guess;
+    }
+};
